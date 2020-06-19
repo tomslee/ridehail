e73b51843585725a0fa31bd66d2de11898fbbf53
Tom Slee <tom.slee@sap.com>
2020-06-19 16:07:49 -0400

    Control simulations using a configuration file
    A standard "ini-style" config file can be used to define simulation
    parameters. config file options can be overwritten on the command line

--- ridehail_animation.py
+++ ridehail_animation.py
@@ -7,7 +7,7 @@ Ridehail animations: for amusement only
 # Imports
 # -------------------------------------------------------------------------------
 import argparse
-# import configparser
+import configparser
 import logging
 import random
 import os
@@ -27,6 +27,7 @@ logger = logging.getLogger(__name__)
 # -------------------------------------------------------------------------------
 # Parameters
 # -------------------------------------------------------------------------------
+
 FRAME_INTERVAL = 50
 AVAILABLE_DRIVERS_MOVING = False
 GARBAGE_COLLECTION_INTERVAL = 10
@@ -37,11 +38,12 @@ DEFAULT_ROLLING_WINDOW = 20
 SUPPLY_RESPONSE_TIME = 5
 EQUILIBRIUM_BLUR = 0.01
 DEFAULT_RESULT_WINDOW = 100
-DEFAULT_MAX_TIME_PERIODS = 1001
+DEFAULT_TIME_PERIODS = 1001
 DEFAULT_REQUEST_RATE = 0.2
-MAX_REQUESTS_PER_PERIOD = 3
 DEFAULT_INTERPOLATION_POINTS = 4
 DEFAULT_CITY_SIZE = 10
+DEFAULT_DRIVER_COUNT = 1
+MAX_REQUESTS_PER_PERIOD = 10
 
 # TODO: IMAGEMAGICK_EXE is hardcoded here. Put it in a config file.
 IMAGEMAGICK_DIR = "/Program Files/ImageMagick-7.0.9-Q16"
@@ -140,6 +142,127 @@ class Equilibration(Enum):
 # ------------------------------------------------------------------------------
 # Classes
 # ------------------------------------------------------------------------------
+class Config():
+    """
+    Hold the configuration parameters for the simulation, which come from three
+    places:
+    - default values, unless overridden by
+    - a configuration file, unless overridden by
+    - command line arguments
+    """
+    def __init__(self, args=None):
+        """
+        Read the configuration file  to set up the parameters
+        """
+        self.config_file = None
+        self.log_level = logging.INFO
+        if args is not None:
+            self.config_file = args.config_file
+            try:
+                if args.verbose:
+                    self.log_level = logging.DEBUG
+                else:
+                    self.log_level = logging.INFO
+            except Exception:
+                self.log_level = logging.INFO
+        config = configparser.ConfigParser()
+        if self.config_file is None:
+            # The default config file is username.config
+            # look for username.config on both Windows (USERNAME)
+            # and Linux (USER)
+            if os.name == "nt":
+                username = os.environ['USERNAME']
+            else:
+                username = os.environ['USER']
+            self.config_file = username + ".config"
+            logger.info(f"Reading configuration file {self.config_file}")
+            if not os.path.isfile(self.config_file):
+                logger.error(
+                    f"Configuration file {self.config_file} not found.")
+        config.read(self.config_file)
+
+        # Fill in individual configuration values
+        # City size
+        self.city_size = int(args.city_size if args.
+                             city_size else config["DEFAULT"]["city_size"])
+        logger.info(f"City size = {self.city_size}")
+        # Driver count
+        self.driver_count = int(args.driver_count if args.driver_count else
+                                config["DEFAULT"]["driver_count"])
+        logger.info(f"Driver count = {self.driver_count}")
+        # Request rate
+        self.request_rate = float(args.request_rate if args.request_rate else
+                                  config["DEFAULT"]["request_rate"])
+        logger.info(f"Request rate = {self.request_rate}")
+        # Time periods
+        self.time_periods = int(args.time_periods if args.time_periods else
+                                config["DEFAULT"]["time_periods"])
+        logger.info(f"Time periods = {self.time_periods}")
+        # Log file TODO not sure if this works
+        self.log_file = str(
+            args.log_file if args.log_file else config["DEFAULT"]["log_file"])
+        logger.info(f"Log file = {self.log_file}")
+        # Verbose output
+        self.verbose = bool(
+            args.verbose if args.verbose else config["DEFAULT"]["verbose"])
+        logger.info(f"Verbose = {self.verbose}")
+        # Quiet output
+        self.quiet = bool(
+            args.quiet if args.quiet else config["DEFAULT"]["quiet"])
+        logger.info(f"Quiet = {self.quiet}")
+        # Show maps or whatever
+        self.show = str(args.show if args.show else config["DEFAULT"]["show"])
+        for option in list(ShowOption):
+            if self.show == option.value:
+                self.show = option
+                break
+        logger.info(f"Show = {self.show}")
+        # Interpolation points
+        self.interpolate = int(args.interpolate if args.interpolate else
+                               config["DEFAULT"]["interpolate"])
+        logger.info(f"Interpolation points = {self.interpolate}")
+        # Equilibrate
+        self.equilibrate = str(args.equilibrate if args.equilibrate else
+                               config["DEFAULT"]["equilibrate"])
+        logger.info(f"Equilibration = {self.equilibrate}")
+        if self.equilibrate:
+            for option in list(Equilibration):
+                if self.equilibrate.lower()[0] == option.name.lower()[0]:
+                    self.equilibrate = option
+                    logger.info(f"Equilibration method is {option.name}")
+                    break
+            if config.equilibrate not in list(Equilibration):
+                logger.error(f"equilibrate must start with s or d")
+        # Price
+        self.price = float(
+            args.price if args.price else config["DEFAULT"]["price"])
+        logger.info(f"Price = {self.price}")
+        # Driver cost
+        self.driver_cost = float(args.driver_cost if args.driver_cost else
+                                 config["DEFAULT"]["driver_cost"])
+        logger.info(f"Driver cost = {self.driver_cost}")
+        # Ride utility
+        self.ride_utility = float(args.ride_utility if args.ride_utility else
+                                  config["DEFAULT"]["ride_utility"])
+        logger.info(f"Ride utility = {self.ride_utility}")
+        # Wait cost
+        self.wait_cost = float(args.wait_cost if args.
+                               wait_cost else config["DEFAULT"]["wait_cost"])
+        logger.info(f"Wait cost = {self.wait_cost}")
+        # Rolling window
+        self.rolling_window = int(args.rolling_window if args.rolling_window
+                                  else config["DEFAULT"]["rolling_window"])
+        logger.info(f"Rolling window = {self.rolling_window}")
+        # Output file for charts
+        self.output = str(
+            args.output if args.output else config["DEFAULT"]["output"])
+        logger.info(f"Output file for charts = {self.output}")
+        # ImageMagick directory
+        self.imagemagick_dir = str(args.imagemagick_dir if args.imagemagick_dir
+                                   else config["DEFAULT"]["imagemagick_dir"])
+        logger.info(f"ImageMagick_Dir = {self.imagemagick_dir}")
+
+
 class Plot():
     """
     Generic Plot class.
@@ -181,41 +304,30 @@ class RideHailSimulation():
     """
     Simulate a ride-hail environment, with drivers and trips
     """
-    def __init__(self,
-                 driver_count,
-                 equilibrate=None,
-                 driver_cost=0.4,
-                 ride_utility=1.0,
-                 wait_cost=1.0,
-                 price=1.0,
-                 request_rate=DEFAULT_REQUEST_RATE,
-                 interpolate=DEFAULT_INTERPOLATION_POINTS,
-                 time_periods=DEFAULT_MAX_TIME_PERIODS,
-                 city_size=DEFAULT_CITY_SIZE,
-                 rolling_window=DEFAULT_ROLLING_WINDOW,
-                 output=None,
-                 show=ShowOption.MAP):
+    def __init__(self, config):
         """
         Initialize the class variables and call what needs to be called.
         The dataframe "data" has a row for each case.
         It must have the following columns:
         - "date_report": the date a case is reported
         """
-        self.driver_count = driver_count
-        self.equilibrate = equilibrate
-        self.driver_cost = driver_cost
-        self.wait_cost = wait_cost
-        self.ride_utility = ride_utility
-        self.price = price
-        self.request_rate = request_rate
-        self.city = City(city_size)
-        self.time_periods = time_periods
-        self.interpolation_points = interpolate
-        self.frame_count = time_periods * self.interpolation_points
-        self.rolling_window = rolling_window
-        self.output = output
-        self.show = show
-        self.drivers = [Driver(i, self.city) for i in range(driver_count)]
+        self.driver_count = config.driver_count
+        self.equilibrate = config.equilibrate
+        self.driver_cost = config.driver_cost
+        self.wait_cost = config.wait_cost
+        self.ride_utility = config.ride_utility
+        self.price = config.price
+        self.request_rate = config.request_rate
+        self.city = City(config.city_size)
+        self.time_periods = config.time_periods
+        self.interpolation_points = config.interpolate
+        self.frame_count = config.time_periods * self.interpolation_points
+        self.rolling_window = config.rolling_window
+        self.output = config.output
+        self.show = config.show
+        self.drivers = [
+            Driver(i, self.city) for i in range(config.driver_count)
+        ]
         self.trips = []
         self.color_palette = sns.color_palette()
         self.stats = {}
@@ -1092,18 +1204,25 @@ def parse_args():
         usage="%(prog)s [options]",
         fromfile_prefix_chars='@')
     parser.add_argument("-c",
+                        "--config_file",
+                        metavar="config_file",
+                        action="store",
+                        type=str,
+                        default=None,
+                        help="""Configuration file""")
+    parser.add_argument("-cs",
                         "--city_size",
                         metavar="city_size",
                         action="store",
                         type=int,
-                        default=DEFAULT_CITY_SIZE,
+                        default=None,
                         help="""Length of the city grid, in blocks.""")
     parser.add_argument("-d",
-                        "--drivers",
-                        metavar="drivers",
+                        "--driver_count",
+                        metavar="driver_count",
                         action="store",
                         type=int,
-                        default=1,
+                        default=None,
                         help="number of drivers")
     parser.add_argument("-eq",
                         "--equilibrate",
@@ -1111,29 +1230,37 @@ def parse_args():
                         type=str,
                         default=None,
                         action="store",
-                        help="Change driver count to equilibrate")
+                        help="""Change driver count  or request rate, or both,
+                        to equilibrate""")
     parser.add_argument("-dc",
                         "--driver_cost",
                         metavar="driver_cost",
                         action="store",
                         type=float,
-                        default=0.4,
+                        default=None,
                         help="""Driver cost per unit time""")
     parser.add_argument("-i",
                         "--interpolate",
                         metavar="interpolate",
                         action="store",
                         type=int,
-                        default=DEFAULT_INTERPOLATION_POINTS,
-                        help="""number of interpolation points when updating
-                        the map""")
-    parser.add_argument("-l",
-                        "--logfile",
-                        metavar="logfile",
+                        default=None,
+                        help="""Number of interpolation points when updating
+                        the map display""")
+    parser.add_argument("-img",
+                        "--imagemagick_dir",
+                        metavar="imagemagick_dir",
                         action="store",
                         type=str,
                         default=None,
-                        help=("logfile name. By default, log messages "
+                        help="""ImageMagick Directory""")
+    parser.add_argument("-l",
+                        "--log_file",
+                        metavar="log_file",
+                        action="store",
+                        type=str,
+                        default=None,
+                        help=("Logfile name. By default, log messages "
                               "are written to the screen only"))
     parser.add_argument(
         "-o",
@@ -1141,17 +1268,18 @@ def parse_args():
         metavar="output",
         action="store",
         type=str,
-        default="",
+        default=None,
         help="""output to the display or as a file; gif or mp4""")
     parser.add_argument("-p",
                         "--price",
                         action="store",
                         type=float,
-                        default=1.0,
+                        default=None,
                         help="Fixed price")
     parser.add_argument("-q",
                         "--quiet",
                         action="store_true",
+                        default=False,
                         help="log only warnings and errors")
     parser.add_argument(
         "-ur",
@@ -1159,21 +1287,21 @@ def parse_args():
         metavar="ride_utility",
         action="store",
         type=float,
-        default=1.0,
+        default=None,
         help="utility of a trip, per period, for the passenger")
     parser.add_argument("-r",
                         "--request_rate",
                         metavar="request_rate",
                         action="store",
                         type=float,
-                        default=DEFAULT_REQUEST_RATE,
+                        default=None,
                         help="requests per period")
     parser.add_argument("-s",
                         "--show",
                         metavar="show",
                         action="store",
                         type=str,
-                        default=ShowOption.MAP,
+                        default=None,
                         help="""show 'all', 'none', 'driver', 'wait',
                         'stats', ['map']""")
     parser.add_argument("-t",
@@ -1181,65 +1309,29 @@ def parse_args():
                         metavar="time_periods",
                         action="store",
                         type=int,
-                        default=DEFAULT_MAX_TIME_PERIODS,
+                        default=None,
                         help="numberof time time periods")
     parser.add_argument("-v",
                         "--verbose",
                         action="store_true",
+                        default=False,
                         help="log all messages, including debug")
-    parser.add_argument("-w",
-                        "--window",
-                        metavar="window",
+    parser.add_argument("-rw",
+                        "--rolling_window",
+                        metavar="rolling_window",
                         action="store",
                         type=int,
-                        default=DEFAULT_ROLLING_WINDOW,
+                        default=None,
                         help="""rolling window for computing averages""")
     parser.add_argument("-wc",
                         "--wait_cost",
                         metavar="wait_cost",
                         action="store",
                         type=float,
-                        default=1,
+                        default=None,
                         help="""Passenger cost per unit wait fraction""")
     args = parser.parse_args()
     return args
-
-
-def validate_args(args):
-    """
-    Check they are OK
-    """
-    valid = True
-    for option in list(ShowOption):
-        if args.show == option.value:
-            args.show = option
-            break
-    if args.equilibrate:
-        for option in list(Equilibration):
-            if args.equilibrate.lower()[0] == option.name.lower()[0]:
-                args.equilibrate = option
-                logger.info(f"Equilibration method is {option.name}")
-                break
-        if args.equilibrate not in list(Equilibration):
-            logger.error(f"equilibrate must start with s or d")
-            valid = False
-    if args.driver_cost:
-        # Must be in (0, 0.5) for an equilibrium to be possible.
-        if args.driver_cost > 0.5 or args.driver_cost < 0:
-            logger.error((f"Driver cost must be in interval (0, 0.5)"))
-            valid = False
-    if valid:
-        return args
-    else:
-        return False
-
-
-def read_config(args):
-    """
-    Take the command line options and read a user-defined config file.
-    Together these define how the program runs.
-    """
-    return "config"
 
 
 def main():
@@ -1253,8 +1345,8 @@ def main():
         loglevel = "WARN"
     else:
         loglevel = "INFO"
-    if args.logfile:
-        logging.basicConfig(filename=args.logfile,
+    if args.log_file:
+        logging.basicConfig(filename=args.log_file,
                             filemode='w',
                             level=getattr(logging, loglevel.upper()),
                             format='%(asctime)-15s %(levelname)-8s%(message)s')
@@ -1263,16 +1355,11 @@ def main():
                             format='%(levelname)-8s%(message)s')
     logger.debug("Logging debug messages...")
     # config = read_config(args)
-    args = validate_args(args)
-    if args is False:
+    config = Config(args)
+    if config is False:
         return False
     else:
-        simulation = RideHailSimulation(args.drivers, args.equilibrate,
-                                        args.driver_cost, args.ride_utility,
-                                        args.wait_cost, args.price,
-                                        args.request_rate, args.interpolate,
-                                        args.time_periods, args.city_size,
-                                        args.window, args.output, args.show)
+        simulation = RideHailSimulation(config)
         simulation.simulate()
 
 
