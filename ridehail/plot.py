9a29e2632c85a00d6f486656441ee5f7d59bc60f
Tom Slee <slee.tom@gmail.com>
2020-08-26 09:12:02 -0400

    formatting tweak

--- ridehail/plot.py
+++ ridehail/plot.py
@@ -70,12 +70,12 @@ class Plot():
         if output.endswith("mp4"):
             writer = FFMpegFileWriter(fps=10, bitrate=1800)
             anim.save(output, writer=writer)
-            del anim    
+            del anim
         elif output.endswith("gif"):
             writer = ImageMagickFileWriter()
             anim.save(output, writer=writer)
-            del anim    
+            del anim
         else:
             plt.show()
-            del anim    
+            del anim
             plt.close()
