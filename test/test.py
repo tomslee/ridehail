20964fa6a627c60ffffe830a366dd843d34a1ae3
Tom Slee <slee.tom@gmail.com>
2020-10-04 20:57:19 -0400

    Random placement test

--- test/test.py
+++ test/test.py
@@ -33,7 +33,7 @@ class TestCity(unittest.TestCase):
             y += loc[1]
         mean_x = x / repeat_count
         mean_y = y / repeat_count
-        print(f"mean position = ({mean_x}, {mean_y})")
+        print(f"\nmean position = ({mean_x}, {mean_y})")
         self.assertAlmostEqual(mean_x + mean_y, expected_magnitude, places=1)
 
     def test_trip_distance(self):
@@ -51,8 +51,44 @@ class TestCity(unittest.TestCase):
             destination = trip.set_destination(origin)
             distance += city.distance(origin, destination)
         mean_distance = distance / repeat_count
-        print(f"mean distance = {mean_distance}")
+        print(f"\nmean distance = {mean_distance}")
         self.assertAlmostEqual(mean_distance, expected_mean_distance, places=1)
+
+    def test_nearest_distance(self):
+        """
+        Place a number of vehicles at random in a city, then see what the
+        distance is to another randome point
+        """
+        idle_vehicle_count = 3
+        request_count = 1000
+        vehicle_locations = []
+        request_locations = []
+        city = atom.City()
+        city.city_size = 50
+        for vehicle in range(idle_vehicle_count):
+            vehicle_locations.append(
+                city.set_random_location(is_destination=False))
+
+        for request in range(request_count):
+            request_locations.append(
+                city.set_random_location(is_destination=False))
+
+        nearest_distances = []
+        for request_location in request_locations:
+            nearest_distance = city.city_size * 2  # big
+            for vehicle_location in vehicle_locations:
+                # distance = (abs(vehicle_location[0] - request_location[0]) +
+                # abs(vehicle_location[1] - request_location[1]))
+                distance = city.distance(request_location, vehicle_location)
+                nearest_distance = min(distance, nearest_distance)
+            nearest_distances.append(nearest_distance)
+
+        mean_nearest_distance = sum(nearest_distances) / request_count
+        print(f"\n'Vehicles': {idle_vehicle_count}"
+              f", 'City size': {city.city_size}"
+              f", 'requests': {request_count}"
+              f", 'mean nearest_distance': {mean_nearest_distance}")
+        self.assertLess(nearest_distance, city.city_size, "too far")
 
 
 if __name__ == '__main__':
