bbc27dfe04b874d3da64b6f4e80c86d6f826b144
Tom Slee <slee.tom@gmail.com>
2021-12-22 07:47:11 -0500

    Tweaking sequence plots: line fitting still not working though

--- plot/rhplotsequence.py
+++ plot/rhplotsequence.py
@@ -40,20 +40,22 @@ def main():
         sequence.append(json.loads(line))
 
     request_rates = list(
-        set([sim["config"]["base_demand"] for sim in sequence if "config" in
-             sim]))
+        set([
+            sim["config"]["base_demand"] for sim in sequence if "config" in sim
+        ]))
     fig, ax = plt.subplots(ncols=1, figsize=(12, 8))
     palette = sns.color_palette()
     for rate in request_rates:
         x = [
             sim["config"]["vehicle_count"] for sim in sequence
-            if sim["config"]["base_demand"] == rate
-            and "config" in sim
+            if sim["config"]["base_demand"] == rate and "config" in sim
         ]
+        print(f"x={x}")
         y1 = [
             sim["results"]["vehicle_fraction_idle"] for sim in sequence
             if sim["config"]["base_demand"] == rate
         ]
+        print(f"y1={y1}")
         y2 = [
             sim["results"]["vehicle_fraction_picking_up"] for sim in sequence
             if sim["config"]["base_demand"] == rate
@@ -69,55 +71,85 @@ def main():
             if sim["config"]["base_demand"] == rate
         ]
         z = zip(x, y1, y2, y3, y4)
-        # Only fit for steady state solutions, where A > 0
+        # Only fit for steady state solutions, where p1 > 0
         z_fit = [zval for zval in z if zval[1] > 0.1]
         if len(z_fit) > 0:
-            (x, y1, y2, y3, y4) = zip(*z_fit)
-        p0_a = y1[-1]
-        p0_b = y1[0] * x[0]
+            (x_fit, y1_fit, y2_fit, y3_fit, y4_fit) = zip(*z_fit)
+        p0_a = y1_fit[-1]
+        p0_b = y1_fit[0] * x_fit[0]
         p0_c = 0
         p0 = (p0_a, p0_b, p0_c)
         try:
-            popt, _ = curve_fit(fit_function, x, y1, p0=p0, maxfev=2000)
-            y1 = [fit_function(xval, *popt) for xval in x]
+            popt, _ = curve_fit(fit_function,
+                                x_fit,
+                                y1_fit,
+                                p0=p0,
+                                maxfev=2000)
+            y1_fit = [fit_function(xval, *popt) for xval in x_fit]
         except Exception:
             pass
-        p0_a = y2[-1]
-        p0_b = y2[0] * x[0]
+        p0_a = y2_fit[-1]
+        p0_b = y2_fit[0] * x_fit[0]
         p0_c = 0
         p0 = (p0_a, p0_b, p0_c)
         try:
-            popt, _ = curve_fit(fit_function, x, y2, p0=p0, maxfev=2000)
-            y2 = [fit_function(xval, *popt) for xval in x]
+            popt, _ = curve_fit(fit_function,
+                                x_fit,
+                                y2_fit,
+                                p0=p0,
+                                maxfev=2000)
+            y2_fit = [fit_function(xval, *popt) for xval in x_fit]
         except Exception:
             pass
-        p0_a = y3[-1]
-        p0_b = y3[0] * x[0]
+        p0_a = y3_fit[-1]
+        p0_b = y3_fit[0] * x[0]
         p0_c = 0
         p0 = (p0_a, p0_b, p0_c)
         try:
-            popt, _ = curve_fit(fit_function, x, y3, p0=p0, maxfev=2000)
-            y3 = [fit_function(xval, *popt) for xval in x]
+            popt, _ = curve_fit(fit_function,
+                                x_fit,
+                                y3_fit,
+                                p0=p0,
+                                maxfev=2000)
+            y3_fit = [fit_function(xval, *popt) for xval in x_fit]
         except Exception:
             pass
-        p0_a = y4[-1]
-        p0_b = y4[0] * x[0]
+        p0_a = y4_fit[-1]
+        p0_b = y4_fit[0] * x_fit[0]
         p0_c = 0
         p0 = (p0_a, p0_b, p0_c)
         try:
-            popt, _ = curve_fit(fit_function, x, y4, p0=p0, maxfev=2000)
-            y4 = [fit_function(xval, *popt) for xval in x]
+            popt, _ = curve_fit(fit_function,
+                                x_fit,
+                                y4_fit,
+                                p0=p0,
+                                maxfev=2000)
+            y4_fit = [fit_function(xval, *popt) for xval in x_fit]
         except Exception:
             pass
         line_style = "solid"
         palette_index = 0
+        # line, = ax.plot(x_fit,
+        # y1_plot,
+        # color=palette[palette_index],
+        # alpha=0.8,
+        # lw=2,
+        # ls=line_style)
+        line, = ax.plot(
+            x,
+            y1,
+            color=palette[palette_index],
+            alpha=0.8,
+            marker="o",
+            markersize=8,
+        )
         line, = ax.plot(x,
                         y1,
                         color=palette[palette_index],
                         alpha=0.8,
                         lw=2,
                         marker="o",
-                        markersize=6,
+                        markersize=8,
                         ls=line_style)
         if rate <= min(request_rates):
             line.set_label("Vehicle idle (p1)")
@@ -128,7 +160,7 @@ def main():
                         alpha=0.8,
                         lw=2,
                         marker="o",
-                        markersize=6,
+                        markersize=8,
                         ls=line_style)
         if rate <= min(request_rates):
             line.set_label("Vehicle dispatch (p2)")
@@ -139,7 +171,7 @@ def main():
                         alpha=0.8,
                         lw=2,
                         marker="o",
-                        markersize=6,
+                        markersize=8,
                         ls=line_style)
         if rate <= min(request_rates):
             line.set_label("Vehicle with rider (p3)")
@@ -150,7 +182,7 @@ def main():
                         alpha=0.8,
                         lw=2,
                         marker="o",
-                        markersize=6,
+                        markersize=8,
                         ls="dashed")
         if rate <= min(request_rates):
             line.set_label("Trip wait time (fraction)")
@@ -169,47 +201,41 @@ def main():
     idle_vehicles_moving = [
         sim["config"]["idle_vehicles_moving"] for sim in sequence
     ][0]
-    results_window = [
-        sim["config"]["results_window"] for sim in sequence
-    ][0]
-    caption = (
-        f"Trip length: [{min_trip_distance}, {max_trip_distance}]\n"
-        f"Trip inhomogeneity: {trip_inhomogeneity}\n"
-        f"Idle vehicles moving: {idle_vehicles_moving}\n"
-        f"Simulation length: {time_blocks} blocks\n"
-        f"Results window: {results_window} blocks\n"
-        f"Generated on {datetime.now().strftime('%Y-%m-%d')}"
-    )
-    anchor_props =  {
+    results_window = [sim["config"]["results_window"] for sim in sequence][0]
+    caption = (f"Trip length: [{min_trip_distance}, {max_trip_distance}]\n"
+               f"Trip inhomogeneity: {trip_inhomogeneity}\n"
+               f"Idle vehicles moving: {idle_vehicles_moving}\n"
+               f"Simulation length: {time_blocks} blocks\n"
+               f"Results window: {results_window} blocks\n"
+               f"Generated on {datetime.now().strftime('%Y-%m-%d')}")
+    anchor_props = {
         'bbox': {
-                'facecolor': 'ghostwhite',
-                'edgecolor': 'silver',
-                'pad': 5,
-            },
+            'facecolor': 'ghostwhite',
+            'edgecolor': 'silver',
+            'pad': 5,
+        },
         'fontsize': 10,
         'linespacing': 2.0
     }
     caption_location = "right"
-    anchored_text = offsetbox.AnchoredText(
-            caption,
-            loc=caption_location,
-            frameon=False,
-            prop=anchor_props
-            )
+    anchored_text = offsetbox.AnchoredText(caption,
+                                           loc=caption_location,
+                                           frameon=False,
+                                           prop=anchor_props)
     ax.add_artist(anchored_text)
     ax.grid(
-            visible=True,
-            which="major",
-            axis="both",
-            # color="black",
-            linewidth="2")
+        visible=True,
+        which="major",
+        axis="both",
+        # color="black",
+        linewidth="2")
     ax.grid(
-            visible=True,
-            which="minor",
-            axis="both",
-            # color="white",
-            linewidth="1")
-        # Minor ticks
+        visible=True,
+        which="minor",
+        axis="both",
+        # color="white",
+        linewidth="1")
+    # Minor ticks
     ax.set_ylim(bottom=0, top=1)
     ax.tick_params(which='minor', bottom=False, left=False)
     ax.xaxis.set_minor_locator(AutoMinorLocator(2))
@@ -226,7 +252,7 @@ def main():
     ax.set_title(title)
     ax.legend()
     plt.savefig(f"img/{filename_root}.png")
-    print (f"Chart saved as img/{filename_root}.png")
+    print(f"Chart saved as img/{filename_root}.png")
 
 
 if __name__ == '__main__':
