11d5ad850aa7491bb1ffa5621983a2feb49ab5a0
Tom Slee <slee.tom@gmail.com>
2023-03-10 11:21:26 -0500

    Added unittest tests to the test directory

--- test/test.py
+++ test/test.py
@@ -1,6 +1,15 @@
+#!/bin/usr/python
+import sys
+
+sys.path.append("/home/tom/src/ridehail")
 import unittest
 import random
-from ridehail import animation, atom, config, simulation, sequence
+
+# from ridehail import animation, atom, config, simulation, sequence
+from ridehail.atom import City, Trip, TripDistribution
+from ridehail.config import RideHailConfig
+from ridehail.simulation import RideHailSimulation
+from ridehail.sequence import RideHailSimulationSequence
 
 
 class TestCity(unittest.TestCase):
@@ -8,10 +17,11 @@ class TestCity(unittest.TestCase):
         """
         Set up a configuration for each test
         """
-        self.rh_config = config.RideHailConfig(use_config_file=False)
-        self.rh_config.city_size = 20
-        self.rh_config.trip_distribution = atom.TripDistribution.UNIFORM
-        self.rh_config.min_trip_distance = 0.0
+        print("\nTest = ", self.id().split("."))  # [-1]
+        self.config = RideHailConfig(use_config_file=False)
+        self.config.city_size = 20
+        self.config.trip_distribution = TripDistribution.UNIFORM
+        self.config.min_trip_distance = 0.0
 
     def tearDown(self):
         pass
@@ -22,13 +32,12 @@ class TestCity(unittest.TestCase):
         so the mean should be (city_size - 0.5) for each coordinate.
         """
         random.seed(0)
-        city = atom.City()
-        city.city_size = self.rh_config.city_size
+        city = City(city_size=self.config.city_size)
         expected_magnitude = city.city_size - 1
         repeat_count = 100000
         (x, y) = (0, 0)
         for i in range(repeat_count):
-            loc = city.set_trip_location(is_destination=False)
+            loc = city.set_location(is_destination=False)
             x += loc[0]
             y += loc[1]
         mean_x = x / repeat_count
@@ -41,12 +50,12 @@ class TestCity(unittest.TestCase):
         set the beginning, and the end, and check the distribution
         """
         random.seed(0)
-        city = atom.City()
+        city = City(city_size=self.config.city_size)
         repeat_count = 10000
         expected_mean_distance = city.city_size / 2.0
         distance = 0
         for i in range(repeat_count):
-            trip = atom.Trip(i, city)
+            trip = Trip(i, city)
             origin = trip.set_origin()
             destination = trip.set_destination(origin)
             distance += city.distance(origin, destination)
@@ -63,15 +72,13 @@ class TestCity(unittest.TestCase):
         request_count = 1000
         vehicle_locations = []
         request_locations = []
-        city = atom.City()
+        city = City(city_size=self.config.city_size)
         city.city_size = 50
         for vehicle in range(idle_vehicle_count):
-            vehicle_locations.append(
-                city.set_trip_location(is_destination=False))
+            vehicle_locations.append(city.set_location(is_destination=False))
 
         for request in range(request_count):
-            request_locations.append(
-                city.set_trip_location(is_destination=False))
+            request_locations.append(city.set_location(is_destination=False))
 
         nearest_distances = []
         for request_location in request_locations:
@@ -84,12 +91,14 @@ class TestCity(unittest.TestCase):
             nearest_distances.append(nearest_distance)
 
         mean_nearest_distance = sum(nearest_distances) / request_count
-        print(f"\n'Vehicles': {idle_vehicle_count}"
-              f", 'City size': {city.city_size}"
-              f", 'requests': {request_count}"
-              f", 'mean nearest_distance': {mean_nearest_distance}")
+        print(
+            f"\n'Vehicles': {idle_vehicle_count}"
+            f", 'City size': {city.city_size}"
+            f", 'requests': {request_count}"
+            f", 'mean nearest_distance': {mean_nearest_distance}"
+        )
         self.assertLess(nearest_distance, city.city_size, "too far")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
